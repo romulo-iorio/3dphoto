988476f253d75683305ef91393f7ac65662b12c7
Rômulo Iorio <romuloiorio@hotmail.com>
2020-07-16 19:39:11 -0300

    19:39

--- publico/scripts/index.js
+++ publico/scripts/index.js
@@ -1,9 +1,5 @@
 const photo3d = document.getElementById("photo3d")
-var orientation = {
-    "alpha": 0,
-    "beta": 90,
-    "gamma": -90
-};
+var orientation = JSON.parse('{"alpha": 0, "beta": 90, "gamma": -90}');
 
 if (window.DeviceOrientationEvent) {
     // Our browser supports DeviceOrientation
