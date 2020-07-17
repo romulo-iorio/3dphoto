b24f56935c3056216b93031373bdb0c9ca711d08
Rômulo Iorio <romuloiorio@hotmail.com>
2020-07-16 21:32:39 -0300

    21:32

--- publico/scripts/index.js
+++ publico/scripts/index.js
@@ -2,10 +2,15 @@ const photo3d = document.getElementById("photo3d")
 const alphaValue = document.getElementById("alpha");
 const betaValue = document.getElementById("beta");
 const gammaValue = document.getElementById("gamma");
+
 var photoPos = {
-    x:  1500, 
-    y:  750
+    x:  1500,       //alpha
+    y:  750         //gamma
 };
+var contIni = 0;
+var alphaIni = 0;
+var betaIni = 0;
+var gammaIni = 0;
 
 if (window.DeviceOrientationEvent) {
     // Our browser supports DeviceOrientation
@@ -22,5 +27,17 @@ function deviceOrientationListener(event) {
     console.log(`gamma: ${event.gamma}`);
     alphaValue.innerText = Math.round(event.alpha);
     betaValue.innerText = Math.round(event.beta);
-    gammaValue.innerText = Math.round(event.gamma);    
+    gammaValue.innerText = Math.round(event.gamma);
+
+    if(!contIni){
+        alphaIni = Math.round(event.alpha);
+        betaIni  = Math.round(event.beta);
+        gammaIni = Math.round(event.gamma);
+        contIni = 1;
+    }else{
+        photoPos.x += (Math.round(event.alpha) - alphaIni);
+        photoPos.y += (Math.round(event.gamma) - gammaIni);     
+        photo3d.style.right = photoPos.x;
+        photo3d.style.bottom = photoPos.y; 
+    }
 }
