48f8f709e3c1185f8d0040aeb08d159a9de78b26
Rômulo Iorio <romuloiorio@hotmail.com>
2020-07-24 06:24:19 -0300

    24/07 06:24

--- publico/scripts/index.js
+++ publico/scripts/index.js
@@ -21,7 +21,7 @@ var gammaOld = 0;
 
 if (window.DeviceOrientationEvent) {
     // Our browser supports DeviceOrientation
-    console.log("Your browser support Device Orientation and is running this sensor");
+    console.log("Your browser supports Device Orientation and is running this sensor");
     window.addEventListener("deviceorientation", deviceOrientationListener);
 } else {
     console.log("Sorry, your browser doesn't support Device Orientation");
@@ -44,15 +44,15 @@ function deviceOrientationListener(event) {
         contIni = 1;
     }else{
         if(((alphaOld - Math.round(event.alpha)) < 100) && ((alphaOld - Math.round(event.alpha)) > -100)){
-            photoPos.x += Math.round((alphaOld - Math.round(event.alpha))*3000/360);
+            photoPos.x += (alphaOld - Math.round(event.alpha))*3000/360;
         }
         if(photoPos.x >= 7180) { //7500
             photoPos.x -= 3000;
-        }else if(photoPos.x <= 1180){ //1500
+        }else if(photoPos.x <= 1180) { //1500
             photoPos.x += 3000;
         }
         if((gammaOld - Math.round(event.gamma) < 50) && ((gammaOld - Math.round(event.gamma)) > -50)){
-            photoPos.y -= Math.round((gammaOld - Math.round(event.gamma))*1500/270);
+            photoPos.y -= (gammaOld - Math.round(event.gamma)*1500/270);
         }
         // if(photoPos.y > 1500) {/
         //     photoPos.y -= 1500;
